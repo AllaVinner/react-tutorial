fb92ae991b313f6b118b736e89f1f526d042b29c
allavinner <joel.w.ottosson@gmail.com>
2023-08-25 17:08:15 +0200

    Initiate Tic Tac Toe App

---
+++ tic-tac-toe/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from 'react';
+
+function Game() {
+    return (
+        <div className="game">
+            Just some intitial text!
+        </div>
+    );
+}
+
+export default Game

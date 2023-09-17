90950d78d9aa90dce48a8dab96b5859bb4fa1fa3
allavinner <joel.w.ottosson@gmail.com>
2023-09-17 14:35:46 +0200

    Redirect page build to tic tac toe

--- tic-tac-toe-generalized/src/App.jsx
+++ tic-tac-toe-generalized/src/App.jsx
@@ -1,78 +1,184 @@
 import { useState } from 'react'
-import './styles.css'
 
-function Box({value, handleClick}) {
+
+function Square({ value, onSquareClick }) {
+  
+  const default_value = ' '
 
   return (
-    <button className='box' onClick={handleClick}>{value}</button>
+    <button
+      className="square"
+      onClick={onSquareClick}
+    >
+      {value ? value: default_value}
+    </button>
   );
 }
 
-function Board({ ncol, nrow, round, nextRound, players}) {
-  let grid = Array(nrow).fill().map(_ => Array(ncol).fill(null));x  
+function Row({rowNumber, array, setCellValue}) {
 
-  function handleClick(rowi, coli) {
-    console.log('in board handle click', rowi, coli, round);
-    grid[rowi][coli] = round % players.length;
-    nextRound();
-    
-  }
-
-  let gridUi = grid.map((row, ri) => {
-    return (
-      <div className='board-row'>
-        {
-          row.map((val, ci) => {
-            return <Box value={val} handleClick={() => handleClick(ri, ci)}/>;
-          })
-        }
-      </div>
-    );
-  });
-
-
-
-  return (
-    <>
-      {gridUi}
-    </>
-  );
+  return <ul key={'rowcontainer' + rowNumber} className="row">
+    {array.map((v, i) => <Square key={'row-'+rowNumber+'col-'+i} value={v} onSquareClick={(e) => setCellValue(i)} />) }
+  </ul>
 }
 
-function BoardConfig({setNCol, setNRow}) {
 
-  function handleInput(e) {
-    console.log('INPTUTTT');
-    console.log(e.target.value);
-    setNRow(e.target.value)
-  }
+function Matrix({ array, setCellValue}) {
+  
   return (
     <>
-      <input type='number' onInput={e => setNRow(e.target.value * 1)}/>
-      <input type='number' onInput={e => setNCol(e.target.value*1)} />
+      {array.map((v, i) => <Row key={'row-number-' + i} rowNumber={i} array={v} setCellValue={(col, val) => setCellValue(i, col)} />)}
     </>
-  );
+  )
 }
 
 function App() {
-  const [ncol, setNCol] = useState(5);
-  const [nrow, setNRow] = useState(4);
-  const [round, setRound] = useState(0);
-  const [players, setPlayers] = useState(['X', 'Y']);
+  const [numRow, setRow] = useState(3)
+  const [numCol, setCol] = useState(3)
+  const [numToWin, setNumToWin] = useState(3)
+  const [moveNum, setMoveNum] = useState(0)
+  const [winningPlayer, setWinningPlayer] = useState(null)
 
-  function nextRound() {
-    setRound(round + 1);
+  const players = ['X', 'O']
+
+  console.log('Run App')
+  const [arr, setArr] = useState(Array(numRow).fill(null).map((v) => Array(numCol).fill(null)))
+
+  const resetBoard = () => {
+    setArr(Array(numRow).fill(null).map((v) => Array(numCol).fill(null)))
+    setWinningPlayer(null)
+  }
+  
+  const handleRowSubmit = (e) => {
+    const num = parseInt(e.target.value)
+    if (num) {
+      setArr(Array(num).fill(null).map((v) => Array(numCol).fill(null)))
+      setRow(num)
+    }
   }
 
-  return (
+  const handleColSubmit = (e) => {
+    const num = parseInt(e.target.value)
+    if (num) {
+      setArr(Array(numRow).fill(null).map((v) => Array(num).fill(null)))
+      setCol(num)
+    }
+  }
+
+  const handleNumToWinSubmit = (e) => {
+    const num = parseInt(e.target.value)
+    if (num) {
+      setNumToWin(num)
+    }
+  }
+
+  const setCellValue = (row, col) => {
+    if (arr[row][col] != null) {
+      return
+    }
+    var tmp = arr.slice()
+    tmp[row][col] = players[moveNum % players.length]
+    if (isWinning(row, col, numToWin, tmp)) {
+      setWinningPlayer(tmp[row][col])
+    }
+    setArr(tmp)
+    setMoveNum(moveNum + 1)
+  }
+
+  return (  
     <>
-      <BoardConfig setNRow={setNRow} setNCol={setNCol} />
-      <div>Num Row {nrow}</div>
-      <div>Num Column {ncol}</div>
-      <div> Hello Tic</div>
-      <Board ncol={ncol} nrow={nrow} round={round} nextRound={nextRound} players={players} />
+      <div className='container'>
+        <div className='header'>
+          <h1>Tic Toe Tac</h1>
+        </div>
+        <div className='sidebar'>
+          <h3 className='config-label'></h3>
+          <button onClick={() => resetBoard()} >Reset</button>
+          <h3 className='config-label'>Number of Rows: </h3>
+          <input type="number" onChange={(e) => handleRowSubmit(e)} value={numRow} />
+          <h3 className='config-label'>Number of Columns: </h3>
+          <input type="number" onChange={(e) => handleColSubmit(e)} value={numCol} />
+          <h3 className='config-label'>Number to Win: </h3>
+          <input type="number" onChange={(e) => handleNumToWinSubmit(e)} value={numToWin} />
+      </div>
+      <div className='board'>
+        <Matrix array={arr} setCellValue={setCellValue} />
+        </div>
+        {winningPlayer &&
+          <h2 className='winnning-header'>Player {winningPlayer} Won!</h2>
+        }
+      </div>
     </>
-  );
+  )
 }
+
+
+function addVec(a, b) {
+  let sum = []
+  for (let i = 0; i < a.length; i++) {
+    sum.push(a[i] + b[i])
+  }
+  return sum
+}
+
+
+function negVec(a) {
+  let neg = []
+  for (let i = 0; i < a.length; i++) {
+    neg.push(-a[i])
+  }
+  return neg
+}
+
+function isWinning(row, col, numToWinn, board) {
+  const player = board[row][col]
+  let numConnected = 1
+  let cursor = [row, col]
+  for (let direction of [[1, 0], [0, 1], [1, 1], [1, -1]]) {
+    numConnected = 1
+    cursor = [row, col]
+    for (let i = 1; i < numToWinn; i++) {
+      cursor = addVec(cursor, direction)
+      if (!isInside(cursor[0], cursor[1], board.length, board[0].length)) {
+        break;
+      }
+      if (board[cursor[0]][cursor[1]] === player) {
+        numConnected += 1
+      } else {
+        break;
+      }
+    }
+
+    cursor = [row, col]
+    direction = negVec(direction)
+    for (let i = 1; i < numToWinn; i++) {
+      cursor = addVec(cursor, direction)
+      if (!isInside(cursor[0], cursor[1], board.length, board[0].length)) {
+        break;
+      }
+      if (board[cursor[0]][cursor[1]] === player) {
+        numConnected += 1
+      } else {
+        break;
+      }
+    }
+    if (numConnected >= numToWinn) {
+      return true
+    }
+  }
+
+  return false  
+}
+
+function isInside(row, col, maxRow, maxCol) {
+  if (row < 0 || col < 0) {
+    return false;
+  }
+  if (row >= maxRow || col >= maxCol) {
+    return false;
+  }
+  return true;
+}
+
 
 export default App
